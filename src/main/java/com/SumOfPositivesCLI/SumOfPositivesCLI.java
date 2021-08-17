5e469337fca9568d9112480c0ea36426b57f6870
Hunter Guthrie <hg7998@yahoo.com>
2021-08-17 10:33:01 -0500

    finished checkpoint

---
+++ src/main/java/com/SumOfPositivesCLI/SumOfPositivesCLI.java
@@ -0,0 +1,25 @@
+package com.SumOfPositivesCLI;
+
+public class SumOfPositivesCLI {
+    public static void main(String[] args) {
+        int sum = 0;
+        if (args.length == 0) {
+            System.out.println(0);
+        }
+        else{
+            for(int i = 0; i <args.length;i++) {
+                try {
+                    int x = Integer.parseInt(args[i]);
+                    if(x > 0) {
+                        sum += x;
+                    }
+                } catch(NumberFormatException e) {
+                    System.out.println("Please enter only numbers.");
+                    return;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+
+}
